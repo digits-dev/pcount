d95d2dd3fe4afcba6d165bab5f33767d74d7f87e
Mike Rodelas <mikerodelas@digits.ph>
2025-04-04 20:55:57 +0800

    add caching to category tags import
    - add cache to remember the previous wh category data

--- app/Imports/UserCategoryTagImport.php
+++ app/Imports/UserCategoryTagImport.php
@@ -9,19 +9,24 @@ use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use CRUDBooster;
+use Illuminate\Support\Facades\Cache;
 
 class UserCategoryTagImport implements ToModel, WithHeadingRow, WithChunkReading
 {
     use Importable;
 
-    public function model(array $row)
-    {
+    public function model(array $row) {
+
+        $warehouseCategory = Cache::remember('warehouse_category'.$row['warehouse_category'], 360, function () use ($row) {
+            return WarehouseCategory::withCategory($row['warehouse_category'])->id;
+        });
+
         UserCategoryTag::updateOrInsert([
             'category_tag_number' => $row['category_tag']
         ],[
             'user_name' => $row['user_name'],
             'category_tag_number' => $row['category_tag'],
-            'warehouse_categories_id' => WarehouseCategory::withCategory($row['warehouse_category'])->id,
+            'warehouse_categories_id' => $warehouseCategory,
             'is_used' => ($row['is_used'] == "YES") ? 1 : 0,
             'status' => $row['status'],
             'created_at' => date('Y-m-d H:i:s'),
@@ -29,8 +34,7 @@ class UserCategoryTagImport implements ToModel, WithHeadingRow, WithChunkReading
         ]);
     }
 
-    public function chunkSize(): int
-    {
+    public function chunkSize(): int {
         return 1000;
     }
 }
