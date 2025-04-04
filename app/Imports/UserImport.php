c16b444724ed094f813e4fb187f0a0db5e43ccd9
Mike Rodelas <mikerodelas@digits.ph>
2025-04-04 20:55:23 +0800

    use caching for user import
    - add cache to remember the previous privilege data

--- app/Imports/UserImport.php
+++ app/Imports/UserImport.php
@@ -9,12 +9,16 @@ use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use CRUDBooster;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
 
 class UserImport implements ToModel, WithHeadingRow, WithChunkReading
 {
-    public function model(array $row)
-    {
+    public function model(array $row) {
+        $privilege = Cache::remember('privilege'.$row['privilege'], 360, function () use ($row) {
+            return UserPrivilege::withName($row['privilege'])->id;
+        });
+
         User::updateOrInsert([
             'email' => $row['email_address']
         ],[
@@ -22,15 +26,14 @@ class UserImport implements ToModel, WithHeadingRow, WithChunkReading
             'user_name' => $row['user_name'],
             'email' => $row['email_address'],
             'photo' => 'uploads/1/2023-01/avatar.jpg',
-            'id_cms_privileges' => UserPrivilege::withName($row['privilege'])->id,
+            'id_cms_privileges' => $privilege,
             'password' => Hash::make('qwerty'),
             'status' => $row['status'],
             'created_at' => date('Y-m-d H:i:s')
         ]);
     }
 
-    public function chunkSize(): int
-    {
+    public function chunkSize(): int {
         return 1000;
     }
 }
